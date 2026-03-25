6b404ace960343205da625e3d3c3ef94b5ba4c8d
Pablo Perez <pablo.perezm@upb.edu.co>
2026-03-25 14:24:35 -0500

    ejenmplo 2 de listas

---
+++ ejemplo2_listas.py
@@ -0,0 +1,16 @@
+# Lista de componentes con sus masas (kg) y posiciones (m)
+componentes = ["motor izquierdo", "motor derecho", "fuselaje", "ala izquierda", "ala derecha", "cola"]
+masas = [1200, 1200, 5000, 800, 800, 600]
+posiciones_x = [2, 2, 0, -2, 2, -6]
+
+# Cálculo del centro de masa en eje X sin list comprehensions
+masa_total = 0
+momento_total = 0
+
+for i in range(len(masas)):
+    masa_total += masas[i]
+    momento_total += masas[i] * posiciones_x[i]
+
+centro_masa_x = momento_total / masa_total
+
+print(f"Centro de masa en eje X: {centro_masa_x:.2f} m")
