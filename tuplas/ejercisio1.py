e2068c8c7c5585b3a0212d72fa0fd864c3babe2b
Pablo Perez <pablo.perezm@upb.edu.co>
2026-04-08 15:30:32 -0500

    reto

---
+++ tuplas/ejercisio1.py
@@ -0,0 +1,4 @@
+## las tuplas son como las listas pero inmutable
+## las tuplas van entre parentesis
+## en caso de que se quiero mostrar una tupla que esta dentro de una lista seria
+## print(lista[indice del elemto de la lista][elemento que esta dentro de la tupla, que la tupla esta a su vez dentro de la lista])
